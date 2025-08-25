576d9dbe09beea0406b6d955b0536d45c8bcf1b6
nohsangwoo <nsgr12@naver.com>
2025-08-26 00:05:45 +0900

    fix: sitemap, robots

---
+++ src/app/robots.ts
@@ -0,0 +1,19 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/', '/admin/', '/_next/', '/.next/'],
+      },
+      {
+        userAgent: 'Googlebot-Image',
+        allow: ['/images/', '/hero-background.jpeg', '/*.jpg', '/*.png', '/*.webp'],
+      },
+    ],
+    sitemap: 'https://www.junilgong.com/sitemap.xml',
+    host: 'https://www.junilgong.com',
+  }
+}
